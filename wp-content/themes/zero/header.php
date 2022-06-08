4594b6660814eb56f39882f055a4c1437ceec793
Vlad Zubovich <vlad@rupor.agency>
2022-06-08 15:18:23 +0300

    update progress

--- wp-content/themes/zero/header.php
+++ wp-content/themes/zero/header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -12,8 +13,9 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
@@ -21,18 +23,39 @@
 </head>
 
 <body>
-	<?php if ( function_exists( 'wp_body_open' ) ) {
+	<?php if (function_exists('wp_body_open')) {
 		wp_body_open();
 	} ?>
 
-<div id="page" class="site sscroll">
-	
-
-	<header id="mastheadx" class="site-header navbar-static-top" role="banner">
-        <!-- menu works -->
-       <?php include_once get_template_directory() . '/inc/menubars/header_nav.php'; ?>
-       <!-- .menu work ends -->
-	</header><!-- #masthead -->
-    
-	<div id="content" class="site-content">
-            
+	<div id="page" class="site sscroll">
+		<header id="mastheadx" class="site-header sticky-top" role="banner">
+			<div class="container-fluid">
+				<div class="site-header__wrapper">
+					<div class="site-header__logo">
+						<a href="/"> <img src="<?= get_template_directory_uri(); ?>/assets/img/logo.svg"><span> OPEN CALL </span></a>
+					</div>
+					<div class="site-header__menu">
+						<?php
+						wp_nav_menu(array(
+							'theme_location'  => 'primary_menu',
+							'container'       => 'div',
+							'depth'           => 4,
+							'fallback_cb'     => 'wp_bootstrap_navwalker::fallback',
+							'walker'          => new wp_bootstrap_navwalker()
+						));
+						?>
+					</div>
+				</div>
+			</div>
+		</header><!-- #masthead -->
+		<div class="bottom-bar fixed-bottom">
+			<div class="container-fluid">
+				<div class="bottom-bar__wrapper">
+					<a href="mailto:support@zero10.app">support@zero10.app</a>
+					<a href="#">Privacy & Legal</a>
+					<a href="#">Cookie policy</a>
+					<a href="#">Terms & Conditions</a>
+				</div>
+			</div>		
+		</div>
+		<div id="content" class="site-content">
---
+++ wp-content/themes/zero/template-page/homepage-st1.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Template Name: Главная, Stage 1 (template-page/homepage-st1.php)
+ */
+
+get_header(); ?>
+
+<section class="homepage__header">
+	<div class="container-fluid h-100">
+		<div class="row border_row h-100">
+			<div class="col-lg-6">
+				<div class="homepage__header-banner">
+					<div class="header-banner__title">
+						<h1 class="title_ui"> We selected 5 most progressive<br> fashion designers from all over<br> the world and digitally recreated<br> their most-wanted pieces. </h1>
+						<a href="#" class="button_ui button_outline">Info and Prizes</a>
+					</div>
+					<div class="header-banner__bottom">
+						<div class="header-banner__bottom-items">
+							<span>1 winner per week</span>
+							<span>4 prizes </span>
+						</div>
+					</div>
+				</div>
+
+			</div>
+			<div class="col-lg-6">
+				<div class="homepage__header-form">
+					<div class="header-form__head">
+						SIGN UP
+					</div>
+					<div class="header-form_body">
+						<div class="header-form_body-title">?Stay tune about the future of AR fashion. Subscribe?</div>
+
+						<form>
+							<input type="text" class="input_ui" name="nickname" placeholder="Name or Nickname">
+							<input type="text" class="input_ui" name="email" placeholder="E-mail">
+							<div class="checkbox_ui">
+								<input type="checkbox" class="custom-checkbox" id="checkbox" name="accept">
+								<label for="checkbox">An end-user license agreement is a legal contract entered into between a software.</label>
+							</div>
+							<input type="submit" class="button_form" value="Apply">
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="homepage__banner">
+	<div class="container-fluid">
+		<img src="<?= get_template_directory_uri(); ?>/assets/img/demo_banner.png" width="100%">
+	</div>
+</section>
+<section class="homepage__about">
+	<div class="container-fluid">
+		<div class="title_ui"> A contemporary fashion brand run by family duo, Ksenia and Anton Schnaider. The masterminds behind the brand are constantly working hand in hand-creating fashion. A fashion brand run by family duo, Ksenia and Anton Schnaider. The masterminds behind the brand are constantly working hand in hand-creating fashion. </div>
+	</div>
+</section>
+<?php
+get_footer();
