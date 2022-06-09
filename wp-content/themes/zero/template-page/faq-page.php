df90c6a97a09f35f0443f0b44966d731ea122218
Anna <anna_p@rupor.agency>
2022-06-09 16:09:55 +0300

    faq-page layout

---
+++ wp-content/themes/zero/template-page/faq-page.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Template Name: F.A.Q. (template-page/faq-page.php)
+ */
+
+get_header(); ?>
+<section class="faq__section">
+    <div class="container-fluid h-100">
+        <div class="row border_row h-100">
+            <div class="col-lg-6">
+                <div class="faq__title">
+                    Frequently <br> Asked Questions
+                </div>
+            </div>
+            <div class="col-lg-6 faq__qestions-block">
+                <div class="faq__qestions-wrapper" id="qestionsWrapper">
+                    <?
+                    $count = 1;
+                    if( have_rows('faq_group') ):
+
+                            // перебираем данные
+                        while ( have_rows('faq_group') ) : the_row();?>
+                            <div class="faq__qestion-wrapper">
+                                <h2 class="qestion-wrapper__header accordion-header" id="questionItem<?=$count ?>">
+                                    <button class="qestion-wrapper__button accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseQestion<?=$count ?>" aria-expanded="true" aria-controls="collapseOne">
+                                        <div class="qestion-wrapper__qestion">
+                                            <p class="qestion__count"><?=$count ?>.</p>
+                                            <p class="qestion__title"><?= the_sub_field('faq_question') ?></p>
+                                        </div>
+                                    </button>
+                                </h2>
+                                <div id="collapseQestion<?=$count ?>" class="accordion-collapse collapse show" aria-labelledby="questionItem<?=$count ?>" data-bs-parent="#qestionsWrapper">
+                                    <div class="qestion-wrapper__body accordion-body">
+                                        <?= the_sub_field('faq_answer') ?>
+                                    </div>
+                                </div>
+                            </div>
+                            <?$count++;?>
+                        <?endwhile;
+                    
+                    else :
+                    
+                    endif;
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+get_footer();
