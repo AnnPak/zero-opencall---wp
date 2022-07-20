a9cfda591995b4be7ad6918ee3e34251e5a8e86d
Anna <anna_p@rupor.agency>
2022-07-20 15:21:04 +0300

    fixed sign in block

--- wp-content/themes/zero/template-parts/content-participants.php
+++ wp-content/themes/zero/template-parts/content-participants.php
@@ -12,7 +12,7 @@
                 <div class="works-auth_text">Just a quick move to leave a vote</div>
                 <div class="works-auth_buttons-auth">
                     <a rel="nofollow" href="/wp-json/wslu-social-login/type/google" class="g-button"><img src="<?= get_template_directory_uri(); ?>/assets/img/icon/google.svg"> Sign in with GOOGLE </a>
-                    <a rel="nofollow" href="/wp-json/wslu-social-login/type/facebook" class="f-button"><img src="<?= get_template_directory_uri(); ?>/assets/img/icon/facebook.png"> Sign in with FACEBOOK </a>
+                    <a rel="nofollow" href="/wp-json/wslu-social-login/type/facebook" class="f-button"><img src="<?= get_template_directory_uri(); ?>/assets/img/icon/fb-icon.svg"> Sign in with FACEBOOK </a>
 
                 </div>
             </div>
@@ -53,7 +53,7 @@
                 <div class="works-auth_text">Just a quick move to leave a vote</div>
                 <div class="works-auth_buttons-auth">
                     <a rel="nofollow" href="/wp-json/wslu-social-login/type/google" class="g-button"><img src="<?= get_template_directory_uri(); ?>/assets/img/icon/google.svg"> Sign in with GOOGLE </a>
-                    <a rel="nofollow" href="/wp-json/wslu-social-login/type/facebook" class="f-button"><img src="<?= get_template_directory_uri(); ?>/assets/img/icon/facebook.png"> Sign in with FACEBOOK </a>
+                    <a rel="nofollow" href="/wp-json/wslu-social-login/type/facebook" class="f-button"><img src="<?= get_template_directory_uri(); ?>/assets/img/icon/fb-icon.svg"> Sign in with FACEBOOK </a>
 
                 </div>
             </div>
