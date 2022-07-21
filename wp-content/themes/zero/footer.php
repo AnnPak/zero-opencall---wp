16a6b680427cb415dc9c172f5c9df9419ff73441
Anna <anna_p@rupor.agency>
2022-07-21 09:59:05 +0300

    fixed video at footer

--- wp-content/themes/zero/footer.php
+++ wp-content/themes/zero/footer.php
@@ -15,9 +15,9 @@
 </div><!-- #content -->
 
 <?php
-	$url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+$url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
 
-	$isStage2 = strpos($url,'test-stejdzh-2') != false;
+$isStage2 = strpos($url, 'test-stejdzh-2') != false;
 
 ?>
 
@@ -26,12 +26,12 @@
 <footer class="footer__bottom">
 	<div class="container-fluid h-100">
 		<div class="row border_row h-100">
-			<div class="col-lg-6">
+			<?php if ($isStage2) : ?>
+				<div class="col-lg-6 p-0">
 
-				<?php if ($isStage2):?>
 					<div class="footer__bottom-wrapper footer__bottom-wrapper_stage2">
 						<div class="title_ui">
-							A contemporary fashion brand run by family duo, Ksenia and Anton Schnaider. The masterminds behind the brand are constantly working hand in hand-creating fashion. 
+							A contemporary fashion brand run by family duo, Ksenia and Anton Schnaider. The masterminds behind the brand are constantly working hand in hand-creating fashion.
 						</div>
 						<div>
 							<div class="footer__bottom-button">
@@ -50,7 +50,53 @@
 						</div>
 
 					</div>
-				<?php else:?>
+					<div class="subscribe-section">
+						<div class="subscribe-section__title">SUBSCRIBE</div>
+
+						<div class="subscribe-section__form-container">
+							<div class="subscribe-section__subtitle">?Stay tune about the future of AR fashion. Subscribe?</div>
+							<?php echo do_shortcode('[contact-form-7 id="63" title="Subscribe html_class="subscribe-form"]'); ?>
+						</div>
+
+						<div class="footer__bottom-contact">
+							<div class="footer__bottom-social">
+								<a href="https://www.instagram.com/zero10.app/">INSTAGRAM</a>
+								<a href="https://www.tiktok.com/@zero10.app">TIKTOK</a>
+								<a href="https://discord.com/invite/zero10?utm_source=stories&utm_medium=link&utm_campaign=discord">DISCORD</a>
+								<a href="https://twitter.com/zero10_app">TWITTER</a>
+							</div>
+							<div class="footer__bottom-contact_mobile">
+								<a href="mailto:opencall@zero10.app">opencall@zero10.app</a>
+								<a href="/privacy-legal">Privacy & Legal</a>
+								<a href="/cookie-policy">Cookie policy</a>
+								<a href="/terms-conditions">Terms & Conditions</a>
+							</div>
+						</div>
+
+
+					</div>
+
+
+				</div>
+
+				<div class="col-lg-6 footer__right-part">
+					<div class="scroll sticky-top">
+						<div class="footer__bottom-img ">
+							<video autoplay="" muted="" loop="" playsinline="" id="videoback" poster="<?= get_template_directory_uri(); ?>/assets/img/video-preview.png">
+								<source src="<?= get_template_directory_uri(); ?>/assets/img/main-1.mp4" type="video/mp4">
+								<source src="<?= get_template_directory_uri(); ?>/assets/img/main-2.webm" type="video/webm">
+							</video>
+						</div>
+					</div>
+
+				</div>
+
+
+
+			<?php else : ?>
+
+				<div class="col-lg-6">
+
 
 					<div class="footer__bottom-wrapper">
 						<div class="title_ui">
@@ -65,9 +111,9 @@
 							<img src="<?= get_template_directory_uri(); ?>/assets/img/qrcode_black-2.svg">
 
 							<span> Download the iOS app </span>
-						
+
 						</a>
-						
+
 						<div class="footer__bottom-qr-mobile">
 							<a href="https://apps.apple.com/us/app/zero10-ar-fashion-platform/id1580413828" class="button_ui button__black">Download on the App Store</a>
 						</div>
@@ -88,20 +134,20 @@
 							<a href="/terms-conditions">Terms & Conditions</a>
 						</div>
 					</div>
-
-					
-				<?php endif;?>
-			</div>
-			
-			<div class="col-lg-6">
-				<div class="footer__bottom-img">
-					<video autoplay="" muted="" loop="" playsinline="" id="videoback" poster="<?= get_template_directory_uri(); ?>/assets/img/video-preview.png">
-						<source src="<?= get_template_directory_uri(); ?>/assets/img/main-1.mp4" type="video/mp4">
-						<source src="<?= get_template_directory_uri(); ?>/assets/img/main-2.webm" type="video/webm">
-					</video>
 				</div>
 
-			</div>
+				<div class="col-lg-6">
+					<div class="scroll">
+						<div class="footer__bottom-img ">
+							<video autoplay="" muted="" loop="" playsinline="" id="videoback" poster="<?= get_template_directory_uri(); ?>/assets/img/video-preview.png">
+								<source src="<?= get_template_directory_uri(); ?>/assets/img/main-1.mp4" type="video/mp4">
+								<source src="<?= get_template_directory_uri(); ?>/assets/img/main-2.webm" type="video/webm">
+							</video>
+						</div>
+					</div>
+				</div>
+			<?php endif; ?>
+
 		</div>
 	</div>
 </footer>
