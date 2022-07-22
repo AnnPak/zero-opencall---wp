ea8c6bf5c694e5f2f721478b9fa1759c5a6e5c6f
Anna <anna_p@rupor.agency>
2022-07-22 12:40:01 +0300

    fixed auth block - stage 2

--- wp-content/themes/zero/template-parts/content-participants.php
+++ wp-content/themes/zero/template-parts/content-participants.php
@@ -11,8 +11,11 @@
                 </div>
                 <div class="works-auth_text">Just a quick move to leave a vote</div>
                 <div class="works-auth_buttons-auth">
-                    <a rel="nofollow" href="/wp-json/wslu-social-login/type/google" class="g-button"><img src="<?= get_template_directory_uri(); ?>/assets/img/icon/google.svg"> Sign in with GOOGLE </a>
-                    <a rel="nofollow" href="/wp-json/wslu-social-login/type/facebook" class="f-button"><img src="<?= get_template_directory_uri(); ?>/assets/img/icon/fb-icon.svg"> Sign in with FACEBOOK </a>
+                        <a rel="nofollow" href="/wp-json/wslu-social-login/type/google" class="g-button d-md-block d-none"><img src="/wp-content/themes/zero/assets/img/icon/google.svg"> Sign in with GOOGLE </a>
+                        <a rel="nofollow" href="/wp-json/wslu-social-login/type/facebook" class="f-button d-md-block d-none"><img src="/wp-content/themes/zero/assets/img/icon/fb-icon.svg"> Sign in with FACEBOOK </a>
+
+                        <a rel="nofollow" href="/wp-json/wslu-social-login/type/google" class="g-button d-md-none d-block"><img src="/wp-content/themes/zero/assets/img/icon/google.svg">  </a>
+                        <a rel="nofollow" href="/wp-json/wslu-social-login/type/facebook" class="f-button d-md-none d-block"><img src="/wp-content/themes/zero/assets/img/icon/fb-icon.svg">  </a>
 
                 </div>
             </div>
@@ -52,8 +55,11 @@
                 </div>
                 <div class="works-auth_text">Just a quick move to leave a vote</div>
                 <div class="works-auth_buttons-auth">
-                    <a rel="nofollow" href="/wp-json/wslu-social-login/type/google" class="g-button"><img src="<?= get_template_directory_uri(); ?>/assets/img/icon/google.svg"> Sign in with GOOGLE </a>
-                    <a rel="nofollow" href="/wp-json/wslu-social-login/type/facebook" class="f-button"><img src="<?= get_template_directory_uri(); ?>/assets/img/icon/fb-icon.svg"> Sign in with FACEBOOK </a>
+                        <a rel="nofollow" href="/wp-json/wslu-social-login/type/google" class="g-button d-md-block d-none"><img src="/wp-content/themes/zero/assets/img/icon/google.svg"> Sign in with GOOGLE </a>
+                        <a rel="nofollow" href="/wp-json/wslu-social-login/type/facebook" class="f-button d-md-block d-none"><img src="/wp-content/themes/zero/assets/img/icon/fb-icon.svg"> Sign in with FACEBOOK </a>
+
+                        <a rel="nofollow" href="/wp-json/wslu-social-login/type/google" class="g-button d-md-none d-block"><img src="/wp-content/themes/zero/assets/img/icon/google.svg">  </a>
+                        <a rel="nofollow" href="/wp-json/wslu-social-login/type/facebook" class="f-button d-md-none d-block"><img src="/wp-content/themes/zero/assets/img/icon/fb-icon.svg">  </a>
 
                 </div>
             </div>
