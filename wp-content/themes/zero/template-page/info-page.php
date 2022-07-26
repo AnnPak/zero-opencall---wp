2aa82007f3e784cf0907e0efcdfd105ca68a981e
Anna <anna_p@rupor.agency>
2022-07-26 13:27:47 +0300

    fixed padding at info section

--- wp-content/themes/zero/template-page/info-page.php
+++ wp-content/themes/zero/template-page/info-page.php
@@ -6,37 +6,6 @@
 
 
 get_header(); ?>
-<!-- 
-<section class="homepage__about info-page">
-	<div class="container-fluid">
-		<div class="title_ui"> A contemporary fashion brand run by family duo, Ksenia and Anton Schnaider. The masterminds behind the brand are constantly working hand in hand-creating fashion. A fashion brand run by family duo, Ksenia and Anton Schnaider. The masterminds behind the brand are constantly working hand in hand-creating fashion. </div>
-		<a href="#" class="button_ui">Check FAQ</a>
-		<div class="about-col">
-			<div class="row">
-				<div class="col-lg-4 col-md-6 col-12">
-					<div class="title_ui">???????</div>
-					<div class="about-col__descript">
-						The AR items bundle by Tommy Cash is united by the surreal infusion of post-Soviet aesthetic and consists of 3 digital items.
-					</div>
-				</div>
-				<div class="col-lg-4 col-md-6 col-12">
-					<div class="title_ui">???????</div>
-					<div class="about-col__descript">
-						The AR items bundle by Tommy Cash is united by the surreal infusion of post-Soviet aesthetic and consists of 3 digital items. The AR items bundle by Tommy Cash is united virginity leaf as a wearable NFT inspired by Adam’s Fig Leaf.
-					</div>
-				</div>
-				<div class="col-lg-4 col-md-6 col-12">
-					<div class="title_ui">???????</div>
-					<div class="about-col__descript">
-						The AR items bundle by Tommy Cash is united by the surreal infusion of post-Soviet aesthetic and consists of 3 digital items and a virginity leaf.
-					</div>
-				</div>
-			</div>
-		</div>
-
-	</div>
-</section> -->
-
 <section class="homepage__banner-background info-page">
 
 	<div class="homepage__banner-title">
